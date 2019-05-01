fd14462823e2d26e00b99f4c370a4c47ddfbdbcf
git-gabriel <fonsecacrz7@gmail.com>
2019-05-01 14:34:38 -0300

    verificação de email simples

---
+++ ProjetoElementary/Elementary/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elementary
+{
+    public partial class Form1 : Form
+    {
+        Medico medico = new Medico();
+        Usuario usuario = new Usuario();
+        BD bd = new BD();
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        //Método construtor que recebe o bd com o novo usuário cadastrado
+        public Form1(BD pBD)
+        {
+            InitializeComponent();
+
+            //Iguala os bds
+            bd = (BD)pBD;
+        }
+
+        // Efeitos da interface
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Email")
+            {
+                textBox1.Clear();
+            }
+
+            textBox1.ForeColor = Color.White;
+            pictureBox2.BackgroundImage = Properties.Resources.icon_email_white;
+            panel2.BackColor = Color.White;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Email";
+            }
+
+            textBox1.ForeColor = Color.Black;
+            pictureBox2.BackgroundImage = Properties.Resources.icon_email_black;
+            panel2.BackColor = Color.Black;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if(textBox2.Text == "Senha")
+            {
+                textBox2.Clear();
+            }
+            
+            textBox2.PasswordChar = '*';
+            textBox2.ForeColor = Color.White;
+            pictureBox3.BackgroundImage = Properties.Resources.icon_pass_white;
+            panel1.BackColor = Color.White;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.PasswordChar = '\u0000';
+                textBox2.Text = "Senha";
+            }
+
+            textBox2.ForeColor = Color.Black;
+            pictureBox3.BackgroundImage = Properties.Resources.icon_pass_black;
+            panel1.BackColor = Color.Black;
+        }
+
+        // Botão sair 'X'
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Botão cadastrar 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Chama a tela de cadastro e passa o bd atual para a tela de cadastro para não perder os dados
+            Form2 cadastrar = new Form2(bd);
+            this.Hide();
+            cadastrar.ShowDialog();
+        }
+
+        // Botão entrar
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (bd.getUsuario(textBox1.Text) != null)
+            {
+                usuario = (Usuario)bd.getUsuario(textBox1.Text);
+
+                if (usuario.getSenha() == textBox2.Text && usuario.getEmail() == textBox1.Text)
+                {
+                    //Chama o formulário do menu
+                    Form3 menu = new Form3(bd, usuario);
+                    this.Hide();
+                    menu.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválido");
+                }
+            }
+            else if (bd.getMedico(textBox1.Text) != null)
+            {
+                medico = (Medico)bd.getMedico(textBox1.Text);
+
+                if (medico.getSenha() == textBox2.Text && medico.getEmail() == textBox1.Text)
+                {
+                    //Chama o formulário do menu
+                    Form3 menu = new Form3(bd,medico);
+                    this.Hide();
+                    menu.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválido");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha inválido");
+            }                  
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Artur Stefan\n\n" +
+                "Cainan\n\n" +
+                "Gabriel Silva Fonseca <fonsecacrz7@gmail.com>\n\n" +
+                "Iuri\n\n" +
+                "Jefferson de lima", "DESENVOLVIDO POR:");
+        }
+    }
+}
---
+++ ProjetoElementary/Elementary/Form3.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elementary
+{
+    public partial class Form3 : Form
+    {
+        Medico medico = new Medico();
+        Usuario usuario = new Usuario();
+        BD bd = new BD();
+
+        public Form3(BD tBd,Medico tMedico)
+        {
+            InitializeComponent();
+
+            bd = (BD)tBd;
+            medico = (Medico)tMedico;
+
+            textBox1.Text = medico.getNome();
+            // Configurações das opções (engrenagem)
+            ActiveControl = pictureBox1;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+        }
+        public Form3(BD tBD,Usuario tUsuario)
+        {
+            InitializeComponent();
+
+            bd = (BD)tBD;
+            usuario = (Usuario)tUsuario;
+
+            textBox1.Text = usuario.getNome();
+            // Configurações das opções (engrenagem)
+            ActiveControl = pictureBox1;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+        }
+
+        // Botão ON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.Silver;
+        }
+
+        // Botão OFF
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Gray;
+            button1.BackColor = Color.Silver;
+        }
+
+        // Efeito pesquisar
+        private void textBox6_Enter(object sender, EventArgs e)
+        {
+            textBox6.Clear();
+            textBox6.ForeColor = Color.White;
+            panel4.BackColor = Color.White;
+            pictureBox1.BackgroundImage = Properties.Resources.icon_magnifier_white;
+        }
+
+        private void textBox6_Leave(object sender, EventArgs e)
+        {
+            panel4.BackColor = Color.Black;
+            textBox6.ForeColor = Color.Black;
+            pictureBox1.BackgroundImage = Properties.Resources.icon_magnifier;
+
+            if (textBox6.Text == "")
+            {
+                textBox6.Text = "Pesquisar";
+            }
+        }
+
+        // Botão sair
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1(bd);
+            this.Hide();
+            form1.ShowDialog();
+        }
+
+        // Exibir/Esconder opções (engrenagem)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button3.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button4.Visible = false;
+            button5.Visible = false;
+            button3.Visible = true;
+            button6.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Artur Stefan\n\n" +
+                "Cainan\n\n" +
+                "Gabriel Silva Fonseca <fonsecacrz7@gmail.com>\n\n" +
+                "Iuri\n\n" +
+                "Jefferson de lima", "DESENVOLVIDO POR:");
+        }
+    }
+}
